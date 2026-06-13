4402fbe373a7028b98cbc34fd76cc6f8dbbe2fb5
Alastor23 <caelantandrews@outlook.com>
2026-06-13 18:19:30 +0000

    Add Hero Component

---
+++ app/frontend/src/components/blocks/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero(props) {
+    const page = props.pageTitle;
+
+    return(
+        <>
+            <section id={`${page}Hero`} className="hero">
+                
+            </section>
+        </>
+    )
+}
--- app/frontend/src/routes/Home.jsx
+++ app/frontend/src/routes/Home.jsx
@@ -1,6 +1,9 @@
+import Hero from "../components/blocks/Hero";
+
 export default function Home() {
     return(
         <>
+            <Hero pageTitle={"home"}/>
         </>
     )
 }
